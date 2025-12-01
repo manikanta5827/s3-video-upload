0e9b2afa0da70d9821089d51f40e967ec6a7bb48
Manikanta Thummuri <postbox5827@gmail.com>
2025-12-01 17:48:11 +0530

    test: mock URL generation in S3 upload unit tests

--- test/unit/s3-upload.test.ts
+++ test/unit/s3-upload.test.ts
@@ -1,11 +1,14 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import {handler} from "../../lambda-functions/s3-upload/app";
+import { handler } from "../../lambda-functions/s3-upload/app";
 import { expect, describe, it } from "@jest/globals";
-import { mockClient } from "aws-sdk-client-mock";
+import * as urlModule from "../../lambda-functions/s3-upload/generateUrl";
 
+jest.mock("../../lambda-functions/s3-upload/generateUrl");
 
-describe('Testing s3 upload file', ()=>{
-    it('s3 pre signed url generation', async()=>{
+describe("Testing s3 upload file", () => {
+  it("s3 pre signed url generation", async () => {
+        (urlModule.generateUrl as jest.Mock).mockResolvedValue("https://mocked-url.com");
+
         
-    })
-})
+  });
+});
