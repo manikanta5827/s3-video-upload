17ffea1196acdc94eaf41ec3393ffb26828b5eb2
Manikanta Thummuri <postbox5827@gmail.com>
2025-12-01 17:48:03 +0530

    refactor: replace S3 signed URL generation with a dedicated function

--- lambda-functions/s3-upload/app.ts
+++ lambda-functions/s3-upload/app.ts
@@ -1,20 +1,13 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-
-const s3 = new S3Client({ region: "ap-south-1" });
-
-const BUCKET_NAME = process.env.BUCKET_NAME;
-
-if (!BUCKET_NAME) throw new Error("BUCKET_NAME is not passed in env");
-
+import { generateUrl } from "./generateUrl";
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   console.log(event);
   let body: { fileName: string } = JSON.parse(event.body as string);
+  let { fileName } = body;
 
-  if (!body.fileName) {
+  if (!fileName || fileName === null) {
     return {
       statusCode: 400,
       body: JSON.stringify({
@@ -23,17 +16,10 @@ export const handler = async (
       }),
     };
   }
-  
-  let fileName = body.fileName;
 
-  const url = await getSignedUrl(
-    s3,
-    new PutObjectCommand({
-      Bucket: BUCKET_NAME,
-      Key: `uploads/${fileName}`,
-    }),
-    { expiresIn: 3600 }
-  );
+  fileName = body.fileName;
+
+  const url = await generateUrl(fileName);
 
   return {
     statusCode: 200,
