5d27d3a8df21f0ebfdb57d7ca67f664af934dd22
Manikanta Thummuri <postbox5827@gmail.com>
2025-12-01 15:37:21 +0530

    first commit

---
+++ lambda-functions/s3-upload/app.ts
@@ -0,0 +1,29 @@
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+
+const s3 = new S3Client({ region: "ap-south-1" });
+
+const BUCKET_NAME = process.env.BUCKET_NAME;
+
+if (!BUCKET_NAME) throw new Error("BUCKET_NAME is not passed in env");
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  let body: { fileName: string } = JSON.parse(event.body as string);
+  let fileName = body.fileName;
+  const url = await getSignedUrl(
+    s3,
+    new PutObjectCommand({
+      Bucket: BUCKET_NAME,
+      Key: `uploads/${fileName}`,
+    }),
+    { expiresIn: 3600 }
+  );
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ url }),
+  };
+};
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+export default {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 2, 
+  useTabs: false,
+  arrowParens: "always"
+};
+
